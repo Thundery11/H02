abd7fc6f622ec207c16c603f4071d1397e9e5d5d
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-09-19 17:50:50 +0300

    q

--- dist/routes/testing-all-data.js
+++ dist/routes/testing-all-data.js
@@ -2,8 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.testingAllDataRouter = void 0;
 const express_1 = require("express");
+const posts_db_1 = require("../db/posts-db");
 exports.testingAllDataRouter = (0, express_1.Router)({});
 exports.testingAllDataRouter.delete('/', (req, res) => {
-    db.length = 0;
+    posts_db_1.postsDb.length = 0;
     res.send(204);
 });
--- src/routes/testing-all-data.ts
+++ src/routes/testing-all-data.ts
@@ -2,7 +2,7 @@ import {Request, Response, Router } from "express";
 import { postsDb } from "../db/posts-db";
 export const testingAllDataRouter = Router({})
 
-testingAllDataRouter.delete('/', (req, res)=>{
-    db.length = 0;
+testingAllDataRouter.delete('/', (req : Request, res: Response)=>{
+    postsDb.length = 0;
 res.send(204)
 })
