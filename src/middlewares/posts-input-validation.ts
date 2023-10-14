ba6e3b3d6542babbbd151fbc6d120c00f0883f2a
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-10-14 17:17:13 +0300

    fix

--- src/middlewares/posts-input-validation.ts
+++ src/middlewares/posts-input-validation.ts
@@ -1,53 +1,52 @@
-import { body } from "express-validator"
-import { blogsRepository } from "../repositories/blogs-db-repository"
+import { body } from "express-validator";
+import { blogsRepository } from "../repositories/blogs-db-repository";
 
+export const postsInputValidation = () => {
+  return [
+    body("title")
+      .trim()
+      .notEmpty()
+      .bail()
+      .isString()
+      .bail()
+      .isLength({ min: 1, max: 30 })
+      .bail()
+      .withMessage("Invalid title"),
 
-export  const postsInputValidation = () =>{
-    return [
-        body('title')
-        .trim()
-        .notEmpty()
-        .bail()
-        .isString()
-        .bail()
-        .isLength({min: 1, max: 30})
-        .bail()
-        .withMessage('Invalid title'),
+    body("shortDescription")
+      .trim()
+      .notEmpty()
+      .bail()
+      .isString()
+      .bail()
+      .isLength({ min: 1, max: 100 })
+      .bail()
+      .withMessage("Invalid title"),
 
-        body('shortDescription')
-        .trim()
-        .notEmpty()
-        .bail()
-        .isString()
-        .bail()
-        .isLength({min: 1, max: 100})
-        .bail()
-        .withMessage('Invalid title'),
+    body("content")
+      .trim()
+      .notEmpty()
+      .bail()
+      .isString()
+      .bail()
+      .isLength({ min: 1, max: 1000 })
+      .bail()
+      .withMessage("Invalid title"),
 
-        body('content')
-        .trim()
-        .notEmpty()
-        .bail()
-        .isString()
-        .bail()
-        .isLength({min: 1, max: 1000})
-        .bail()
-        .withMessage('Invalid title'),
+    body("blogId")
+      .trim()
+      .notEmpty()
+      .bail()
+      .isString()
+      .bail()
+      // .custom(async id =>{
+      //     const blogIsExist =  await blogsRepository.findBlog(id)
 
-        body('blogId')
-        .trim()
-        .notEmpty()
-        .bail()
-        .isString()
-        .bail()
-        .custom(async id =>{
-            const blogIsExist =  await blogsRepository.findBlog(id)
-
-            if(!blogIsExist){
-                throw new Error('Blog doesnt exist')
-            } 
-            return true
-        })
-        .withMessage('Invalid blogId'),
-    ]
-}
+      //     if(!blogIsExist){
+      //         throw new Error('Blog doesnt exist')
+      //     }
+      //     return true
+      // })
+      .withMessage("Invalid blogId"),
+  ];
+};
