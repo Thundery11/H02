ee5763df8fd7cb460d947135bbeac79bfaccb989
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-11-26 15:13:13 +0300

    fix

--- src/routes/auth-router/auth-router.ts
+++ src/routes/auth-router/auth-router.ts
@@ -115,15 +115,11 @@ authRouter.post(
     }
   }
 );
-authRouter.post(
-  "/logout",
-  checkRefreshToken,
-  async (req: Request, res: Response) => {
-    const refreshToken = req.cookies.refresh;
-    await sesionService.updateBlackListTokens(refreshToken);
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-  }
-);
+authRouter.post("/logout", async (req: Request, res: Response) => {
+  const refreshToken = req.cookies.refresh;
+  await sesionService.updateBlackListTokens(refreshToken);
+  res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
 
 authRouter.get("/me", authMiddleware, async (req: Request, res: Response) => {
   const userId = req.user?.id;
